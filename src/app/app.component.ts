b25281c2a67bb4d8111b57adefc841f4f858ef71
dayvidwhy <dayvidwhy@gmail.com>
2024-04-14 12:44:18 +1000

    Lists notes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,11 @@ import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { CommonModule } from "@angular/common";
 
+interface NoteBook {
+    title: string;
+    notes: string[];
+    id: string;
+}
 @Component({
     selector: "app-root",
     standalone: true,
@@ -11,10 +16,29 @@ import { CommonModule } from "@angular/common";
 })
 export class AppComponent {
     title = "quick-glance";
-    items: string[] = [];
 
-    addItem () {
-        console.log("Adding an item");
-        this.items.push("Item " + this.items.length);
+    currentNoteBook: NoteBook | null = null;
+
+    notebooks: NoteBook[] = [];
+
+    addNotebook () {
+        const notebookId = crypto.randomUUID();
+        const newNotebook = {
+            title: "Notebook",
+            notes: [],
+            id: notebookId
+        };
+        this.notebooks.push(newNotebook);
+        this.currentNoteBook = newNotebook;
+    }
+
+    setNotebook (notebook: NoteBook) {
+        this.currentNoteBook = notebook;
+    }
+
+    addNote () {
+        if (this.currentNoteBook) {
+            this.currentNoteBook.notes.push("new note");
+        }
     }
 }
